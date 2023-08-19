75e8c1399289060961cfe2c1e7bc68c39d401afb
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-08-19 12:26:55 +0100

    Began modularisation of my JavaScript. Site-wide functions are in  and page-specific ones will be like . Removed old CSS files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,6 @@
 window.onload = function() {
 	navExpander();
 	navScrollResize();
-	
-	// Delay the execution of typeAndDeleteStrings(stringsArray) by 2 seconds, to allow for other animations to occur first
-    setTimeout(function() {
-		const animationContainer = document.getElementById("animation-container");
-		// Style animation container
-		animationContainer.style.opacity = 1;
-		animationContainer.style.minHeight = 100 + "px";
-        typeAndDeleteStrings(stringsArray);
-    }, 4500);
 };
 
 function navExpander() {
@@ -77,57 +68,4 @@ function navScrollResize() {
 	  // Update the previous scroll position
 	  previousScrollPos = currentScrollPos;
 	});
-  }
-
-
-function typeAndDeleteStrings(stringsArray) {
-	const outputElement = document.getElementById("output");
-
-	// Speed settings for typing, deletion, and pauses (in milliseconds)
-	const typingSpeed = 75;    // Speed at which characters are typed
-	const deletionSpeed = 50;   // Speed at which characters are deleted
-	const pauseDuration = 1000; // Duration to pause between animations
-	const rewriteDuration = 200; // Duration to pause between deletion and writing a enw string
-
-	// Function to type a given string on screen
-	function typeString(str) {
-		let currentCharIndex = 0;
-
-		// Recursive function to type the next character
-		function typeNextChar() {
-			if (currentCharIndex < str.length) {
-				outputElement.textContent += str[currentCharIndex];
-				currentCharIndex++;
-
-				setTimeout(typeNextChar, typingSpeed);
-			} else {
-				setTimeout(deleteString, pauseDuration);
-			}
-		}
-
-		typeNextChar();
-	}
-
-	// Function to delete the typed string from the output element
-	function deleteString() {
-		const currentContent = outputElement.textContent;
-
-		if (currentContent.length > 0) {
-			outputElement.textContent = currentContent.slice(0, -1);
-
-			setTimeout(deleteString, deletionSpeed);
-		} else {
-			// Reset currentIndex to 0 if all strings are completed
-			currentIndex = (currentIndex + 1) % stringsArray.length;
-
-			setTimeout(() => {
-				typeString(stringsArray[currentIndex]);
-			}, pauseDuration);
-		}
-	}
-
-  let currentIndex = 0;
-  typeString(stringsArray[currentIndex]);
 }
-
-const stringsArray = ["develop interesting code.", "learn new skills.", "do other fun stuff lol."];
