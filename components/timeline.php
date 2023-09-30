4097786c9ea7e9ef0baa428c10a4502cda48a3b5
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-09-30 01:22:42 +0100

    Began creating a timeline for my about page, with tooltips using http://www.menucool.com/tooltip/css-tooltip

---
+++ components/timeline.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="../css/timeline.css" />
+<div class="timeline">
+    <div class="event" data-start-date="2023-01-01" data-end-date="2023-02-01">
+        <div class="icon">
+            <div class="tooltip2">
+                <div class="right">
+                    <div class="tooltip-header">
+                        <h3>Previous Jobs</h3>
+                    </div>
+                    <div class="text-content">
+                        <ul>
+                            <li>Test content</li>
+                            <li>Test</li>
+                            <li>Content</li>
+                        </ul>
+                    </div>
+                    <i></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="event" data-start-date="2023-02-15" data-end-date="2023-03-15">
+        <div class="icon"></div>
+    </div>
+</div>
+<script type="text/javascript" src="js.js"></script>
