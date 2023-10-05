a108a1ca65fee238e8e20ad4a4062bde66991cda
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-10-05 22:32:19 +0100

    Added a left tooltip and added dates to timeline. Made minor tweaks to how tooltips display.

--- components/timeline.php
+++ components/timeline.php
@@ -1,9 +1,61 @@
 <link rel="stylesheet" href="../css/timeline.css" />
 <div class="timeline">
-    <div class="event" data-start-date="2023-01-01" data-end-date="2023-02-01">
+    <div class="event" data-start-date="1998-09-04" data-end-date="2018-11-01">
         <div class="icon">
-            <div class="tooltip2">
+            <div class="tooltip">
                 <div class="right">
+                    <div class="tooltip-header">
+                        <h3>Previous Jobs</h3>
+                    </div>
+                    <div class="text-content">
+                        <ul>
+                            <li>Shopkeeper @ Flix DVD & Game Rental</li>
+                            <li>Cashier @ Cocoa Bean Chocolate Factory</li>
+                            <li>Cashier @ Moore's Fish & Chips</li>
+                            <li>Shopkeeper @ DayToDay Express</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="event-date">
+            <h4>2016<br>—<br>2018</h4>
+        </div>
+    </div>
+    <div class="event" data-start-date="2018-11-01" data-end-date="2021-05-01">
+        <div class="icon">
+        <div class="tooltip">
+                <div class="right">
+                    <div class="tooltip-header">
+                        <h3>Customer Service Advisor</h3>
+                        <h4>WebHelp</h4>
+                    </div>
+                    <div class="text-content">
+                        <p>
+                            I was the first point of contact for all Vodafone business customers, eventually moving to Vodafone Prime.
+                            Here, I provided expert-level investigation to complex complaint situations.
+                             Skills used:
+                        </p>
+                        <ul>
+                            <li>Analyticical thinking</li>
+                            <li>Analyticical thinking</li>
+                            <li>Todo: LIST MORE!!</li>
+                        </ul>
+                        <p>
+                            It was while working this role that I decided to pursue my dream career and began studying for my Computing degree.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="event-date">
+            <h4><br>2018</h4>
+        </div>
+    </div>
+    <div class="event" data-start-date="2023-02-15" data-end-date="2023-03-15">
+        <div class="icon">
+        <div class="tooltip">
+                <div class="left">
                     <div class="tooltip-header">
                         <h3>Previous Jobs</h3>
                     </div>
@@ -14,13 +66,34 @@
                             <li>Content</li>
                         </ul>
                     </div>
-                    <i></i>
                 </div>
             </div>
         </div>
+        <div class="event-date">
+            <h4><br>2021</h4>
+        </div>
     </div>
     <div class="event" data-start-date="2023-02-15" data-end-date="2023-03-15">
-        <div class="icon"></div>
+        <div class="icon">
+        <div class="tooltip">
+                <div class="left">
+                    <div class="tooltip-header">
+                        <h3>Previous Jobs</h3>
+                    </div>
+                    <div class="text-content">
+                        <ul>
+                            <li>Test content</li>
+                            <li>Test</li>
+                            <li>Content</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="event-date">
+            <h4>2022<br>—<br>Now</h4>
+        </div>
     </div>
+
+    <div id="timeline-line"> </div>
 </div>
-<script type="text/javascript" src="js.js"></script>
