d174044964f6d3828eee537c0008b0471280abd8
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-10-10 23:37:29 +0100

    Got contact page working for sending emails

--- contact.php
+++ contact.php
@@ -1,3 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $from = "enquiries@alex-mccaughran.net";
+    $to = "hello@alex-mccaughran.net";
+    $message = "Name: " . $_POST['name'] . "\n";
+    $message .= "Email/Phone: " . $_POST['contact'] . "\n";
+    $message .= "Purpose: " . $_POST['purpose'] . "\n";
+    $message .= "Message:\n" . $_POST['message'];
+    $headers = "From:" . $from;
+    $subject = "Contact Form Submission - " . $_POST['purpose'];
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Thank you for your message. I will be sure to get back to you soon!";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -22,7 +41,7 @@
                 <h1 class="section-title">If you like what you see, do get in touch!</h1>
                 <h3>You can either reach me at <a class="about-link" href="mailto:someone@yoursite.com">hello@alex-mccaughran.net</a>, or use the form below to get in touch.</h3>
                 <div id="contact-form-container">
-                    <form id="contact-form" action="submit.php" method="POST">
+                    <form id="contact-form" method="POST">
                         <div class="contact-row">
                             <label for="name">Name:</label>
                             <input type="text" id="form-name" name="name" required>
