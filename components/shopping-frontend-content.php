c77157778ff8373103528ba922122ae097897cfd
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-11-07 21:56:06 +0000

    Added Javascript to cycle between front/back-end showcases and added a 'coming soon' message to shoppnig list page. Created a WIP page to work on, in the mean time, while the 'coming soon' page will be published

--- components/shopping-frontend-content.php
+++ components/shopping-frontend-content.php
@@ -1,3 +1,15 @@
-<div>
-    lol
+<div class="code-block">
+                    <pre><code class="language-js">if ($shadyUrl !== false) {
+    $originalURL = urldecode($shadyUrl);
+
+    // Display the redirect button within the center of the page
+    echo '&ltdiv id="center-button">&lta id="shady-link" href="http://' . $originalURL . '">&ltbutton id="success">Claim Gift&lt/button>&lt/a>&lt/div>';
+    } else {
+    echo '&ltdiv id="center-button">&ltbutton id="failure">This link has expired!&lt/button>&lt/div>';
+}</code></pre>
+    <p>
+        A button is displayed in the center of the viewport.
+    <br><br>If the original URL could be found in the database, the button appears green and the original URL is added itno the href attribute of the anchor around the button.
+    <br><br>If it can't be found, the button will display as red and say a generic "link has expired" message.
+    </p>
 </div>
--- js/projects-scripts.js
+++ js/projects-scripts.js
@@ -1,4 +1,18 @@
 window.onload = function() {
-	navExpander();
-	navScrollResize();
+    const tabs = document.querySelectorAll(".tabs .tab");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", (e) => {
+            e.preventDefault(); // Prevent the default behavior of the link
+            tabs.forEach((t) => t.classList.remove("active"));
+            tabContents.forEach((content) => content.classList.remove("active"));
+
+            tab.classList.add("active");
+            tabContents[index].classList.add("active");
+        });
+    });
+
+    // Activate the first tab and content by default
+    tabs[0].click();
 };
